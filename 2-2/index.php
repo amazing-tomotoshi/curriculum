144e593d976d743d2ff3cf8fabda3d72694c0cb2
amazing-tomotoshi <tomotoshi.2828@ruri.waseda.jp>
2020-08-06 22:38:48 +0900

    変数とデータ型
    index.php
    の作成

---
+++ 2-2/index.php
@@ -0,0 +1,15 @@
+<?php
+$message='Hello World!';
+echo $message;
+echo '<br>';
+$x = 8;
+$y = 5.55;
+var_dump($x);
+echo '<br>';
+echo $y;
+echo '<br>';
+var_dump('耳たぶ');
+echo '<br><br>';
+echo "API:Application Programming Interfaceの略。ソフトウェアからOSの機能を利用するための仕様またはインターフェースの総称で、アプリケーションの開発を容易にするためのソフトウェア資源のことをいう。<br>";
+echo 'SVN：集中型バージョン管理システムのひとつ。';
+?>

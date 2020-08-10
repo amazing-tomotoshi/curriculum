46a280b3c8dfb6b5047e85b9da37666a3c4b36c2
amazing-tomotoshi <tomotoshi.2828@ruri.waseda.jp>
2020-08-10 17:43:36 +0900

    2-13
    index.php
    の作成

---
+++ 2-13/index.php
@@ -0,0 +1,34 @@
+<?php
+echo ceil(3.4);
+echo '<br>';
+echo floor(5.3);
+echo '<br>';
+echo round(6.6);
+echo '<br>';
+echo pi();
+echo '<br>';
+echo mt_rand(55,86);
+echo '<br>';
+echo strlen('group');
+echo '<br>';
+echo strpos('blue','u');
+echo '<br>';
+echo substr('sleepy',1,3);
+echo '<br>';
+echo str_replace('t','T','tokyo');
+echo '<br>';
+printf("%sは%d歳です",'andy', 13);
+echo '<br>';
+echo sprintf("%sは%02d月%02d日生まれだ",'andy',9,3);
+
+echo '<br><br>';
+echo '1. PostgreSQL・Oracle SQL<br>';
+echo 'PostgreSQL：リレーショナルデータベース（RDB）の作成や操作、管理ができるオープンソースのデータベース管理システム（DBMS）の一つ。<br>';
+echo 'Oracle SQL：Oracle Database向けのデータベース操作用プログラミング言語。<br>';
+echo '2. TortoiseGit・TortoiseSVN<br>';
+echo 'TortoiseGit：分散型バージョン管理システムである Git のクライアントで、TortoiseSVNをもとに、 Microsoft Windows シェル拡張 として実装されている。<br>';
+echo 'TortoiseSVN：Subversion のクライアントで、Windowsのシェルエクステンションとして機能する。<br>';
+echo '3. 外部設計・内部設計<br>';
+echo '外部設計：ソフトウェアや情報システムの開発工程の一つで、外部から見たシステムの仕様を定義する工程。<br>';
+echo '内部設計：ソフトウェアや情報システムの開発工程の一つで、全体の構成や行うべき処理の詳細など実装に必要な仕様を定義する工程。<br>';
+?>
